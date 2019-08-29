8d06f2b6b667fadf582b324dd1ce5e011ca3ba47
R0bobot <54596926+R0bobot@users.noreply.github.com>
2019-08-28 23:43:27 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+function setup(){
+    createCanvas(800, 600);
+    background(0, 144, 111);
+    
+    fill(0);
+    
+    strokeWeight(24);
+    stroke(255);
+    ellipse(800, 600, 2140, 2140);
+    
+    strokeWeight(22);
+    stroke(255);
+    ellipse(800, 600, 1860, 1860);
+    
+    strokeWeight(20);
+    stroke(255);
+    ellipse(800, 600, 1600, 1600);
+    
+    strokeWeight(18);
+    stroke(255);
+    ellipse(800, 600, 1360, 1360);
+    
+    strokeWeight(16);
+    stroke(255);
+    ellipse(800, 600, 1140, 1140);
+    
+    strokeWeight(14);
+    stroke(255);
+    ellipse(800, 600, 940, 940);
+    
+    strokeWeight(12);
+    stroke(255);
+    ellipse(800, 600, 760, 760);
+    
+    strokeWeight(10);
+    stroke(255);
+    ellipse(800, 600, 600, 600);
+    
+    strokeWeight(8);
+    stroke(255);
+    ellipse(800, 600, 460, 460);
+    
+    strokeWeight(6);
+    stroke(255);
+    ellipse(800, 600, 340, 340);
+    
+    strokeWeight(4);
+    stroke(255);
+    ellipse(800, 600, 240, 240);
+    
+    strokeWeight(3);
+    stroke(255);
+    ellipse(800, 600, 160, 160);
+    
+    //noStroke();
+    ellipse(800, 600, 100, 100);
+
+    strokeWeight(2);
+    stroke(255);
+    ellipse(800, 600, 60, 60);
+
+}
+function draw(){
+
+}
